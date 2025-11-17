9cc36b1d1914af913f00044d74496316ddafa668
185august <augusteide98@hotmail.com>
2025-11-17 11:33:56 +0100

    Refactored backend to return all graphs as JSON objects and updated route from `/image` to `/charts`. Added support for animations alongside still graphs.

--- backend.py
+++ backend.py
@@ -1,7 +1,10 @@
+import json
 import urllib.parse
-from flask import Flask, jsonify
-import  data_still_grafs
+from fileinput import filename
+
+import  overall_team_statistics_graphs
 import data_animation
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,12 +15,16 @@ from flask import url_for
 def index():
     return 'index'
 
-@app.route('/image')
-def get_image():
-    string = data_still_grafs.top_20_win_percentage()
-    image = 'data:image/png;base64,' + urllib.parse.quote(string)
-    filename = 'top_20_win_percentage.png'
-    return jsonify({'image': image, 'filename': filename})
+@app.route('/charts')
+def get_charts():
+    # string = data_still_grafs.top_20_win_percentage()
+    # fig = data_still_grafs.create_a_bar_graph(data_x=[1, 2, 3, 4, 5], data_y=[0, 1, 2, 3, 4], title='Test', x_label='X', y_label='Y')
+    # string = data_still_grafs.export_bar_graph(fig)
+    figures = overall_team_statistics_graphs.get_all_still_graphs()
+    figures.append(data_animation.get_all_animations())
+    list_of_figures_dicts = [obj.to_dict() for obj in figures]
+
+    return json.dumps(list_of_figures_dicts)
 
 @app.route('/animation')
 def get_animation():
